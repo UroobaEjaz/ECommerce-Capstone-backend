624062f6d0280cdfcb0531b70f7f30415f842499
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2024-05-14 14:49:25 -0600

    components

--- frontend/ecom/app/page.js
+++ frontend/ecom/app/page.js
@@ -1,56 +1,12 @@
+
 import Image from "next/image";
+import shoppingCart from "../components/shoppingCart.jsx";
+import logIn from "../components/login.jsx";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold">Sign Up</h1>
-
-        <form className="w-full space-y-4">
-          <div className="flex space-x-4">
-            <input
-              type="text"
-              placeholder="First Name"
-              className="w-1/2 px-4 py-2 border border-gray-300 rounded-md "
-            />
-            <input
-              type="text"
-              placeholder="Last Name"
-              className="w-1/2 px-4 py-2 border border-gray-300 rounded-md"
-            />
-          </div>
-
-          <div>
-            <input
-              type="text"
-              placeholder="Username"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md "
-            />
-          </div>
-
-          <div>
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md"
-            />
-          </div>
-
-          <div>
-            <input
-              type="text"
-              placeholder="Phone Number"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md "
-            />
-          </div>
-
-          <div>
-            <button className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600 ">
-              Create Account
-            </button>
-          </div>
-        </form>
-      </div>
-    </main>
+    <div>
+      <shoppingCart />
+    </div>
   );
 }
