4ac30cfbbd9a7bef751742aa7a033cc65da316fc
mohjeet3004 <141950821+mohjeet3004@users.noreply.github.com>
2024-05-14 14:54:16 -0600

    login component

--- frontend/ecom/app/page.js
+++ frontend/ecom/app/page.js
@@ -1,12 +1,12 @@
-
 import Image from "next/image";
 import shoppingCart from "../components/shoppingCart.jsx";
-import logIn from "../components/login.jsx";
+import logIn from "../components/SignUpForm.jsx";
+import SignUpForm from "../components/SignUpForm.jsx";
 
 export default function Home() {
   return (
-    <div>
-      <shoppingCart />
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignUpForm />
+    </main>
   );
 }
