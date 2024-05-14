58f9e85758b4a3e2968e046410575cf7cfb7d138
mohjeet3004 <141950821+mohjeet3004@users.noreply.github.com>
2024-05-14 15:15:55 -0600

    Update page.js
    removed an error in shopping kart

--- frontend/ecom/app/page.js
+++ frontend/ecom/app/page.js
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import shoppingCart from "../components/shoppingCart.jsx";
 import logIn from "../components/SignUpForm.jsx";
 import SignUpForm from "../components/SignUpForm.jsx";
- 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
