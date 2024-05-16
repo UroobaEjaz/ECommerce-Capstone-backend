21ba6de2e0de14daf615240b38f474b6d263edd3
mohjeet3004 <141950821+mohjeet3004@users.noreply.github.com>
2024-05-16 14:36:47 -0600

    put the refrence

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-md p-4 flex justify-between items-center fixed top-0 left-0 w-full z-10">
+    // used chatgpt to get the syntax on navbar url="build a simple navbar "
+    <nav className="bg-white shadow-md p-4 flex justify-between items-center fixed top-0 left-0 w-full z-10 ">
+      {/* searched on chat gpt for the tailwind and logo syntax url="https://chatgpt.com/c/a8500a72-5c40-4bb6-af94-9817af802cee"*/}
       <Link to="/" className="">
-        <img src="/logo.jpg" alt="Logo" />
+        <img src="/logo.jpg" alt="Logo " className="w-12 rounded-full" />
       </Link>
       <ul className="flex space-x-4">
         <li>
