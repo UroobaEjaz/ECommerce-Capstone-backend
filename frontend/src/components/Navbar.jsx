2203c2238e4e8959a94bbf55a0103b4409841b5f
Gurjagjot <gurjagjot@gmail.com>
2024-05-16 14:22:13 -0600

    minor improvements

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md p-4 flex justify-between items-center fixed top-0 left-0 w-full z-10">
-      <Link to="/login" className="">
-        <img src="../images/logo.jpg" alt="Logo" />
+      <Link to="/" className="">
+        <img src="/logo.jpg" alt="Logo" />
       </Link>
       <ul className="flex space-x-4">
         <li>
@@ -23,9 +23,9 @@ const Navbar = () => {
           </a>
         </li>
         <li>
-          <a href="/login" className="text-gray-600 hover:text-blue-600">
+          <Link href="/login" className="text-gray-600 hover:text-blue-600">
             Login
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
