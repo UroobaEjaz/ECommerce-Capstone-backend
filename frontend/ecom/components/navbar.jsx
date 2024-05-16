3e1678a805020586ca085330ac9ce25b272afc32
mohjeet3004 <141950821+mohjeet3004@users.noreply.github.com>
2024-05-15 18:36:55 -0600

    changed a tags to link tags

--- frontend/ecom/components/navbar.jsx
+++ frontend/ecom/components/navbar.jsx
@@ -3,29 +3,29 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md p-4 flex justify-between items-center fixed top-0 left-0 w-full z-10">
-      <a href="/" className="text-xl font-bold text-gray-800">
+      <Link href="/" className="text-xl font-bold text-gray-800">
         Logo
-      </a>
+      </Link>
       <ul className="flex space-x-4">
         <li>
-          <a href="/" className="text-gray-600 hover:text-blue-600">
+          <Link href="/" className="text-gray-600 hover:text-blue-600">
             Home
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/about" className="text-gray-600 hover:text-blue-600">
+          <Link href="/about" className="text-gray-600 hover:text-blue-600">
             About
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/contact" className="text-gray-600 hover:text-blue-600">
+          <Link href="/contact" className="text-gray-600 hover:text-blue-600">
             Contact
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/login" className="text-gray-600 hover:text-blue-600">
+          <Link href="/login" className="text-gray-600 hover:text-blue-600">
             Login
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
