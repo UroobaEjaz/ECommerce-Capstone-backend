0fa50b22188ede5223a117af425851650fceb88e
mohjeet3004 <141950821+mohjeet3004@users.noreply.github.com>
2024-05-28 14:11:33 -0600

    updated nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
     // </nav>
 
     /* searched on chat gpt for the tailwind and logo syntax url="https://chatgpt.com/c/a8500a72-5c40-4bb6-af94-9817af802cee"*/
-    <nav className="bg-white shadow-md p-4 flex justify-between items-center fixed top-0 left-0 w-full z-10 ">
+    <nav className="bg-white shadow-md p-4 flex justify-between items-center fixed top-0 left-0 w-full z-10 font-bold">
       <Link to="/" className="">
         <img src="/logo.jpg" alt="Logo " className="w-12 rounded-full" />
       </Link>
