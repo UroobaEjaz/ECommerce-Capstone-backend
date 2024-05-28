6b5b4caf22650c4f6bcbbc6e41ea84612a425812
mohjeet3004 <141950821+mohjeet3004@users.noreply.github.com>
2024-05-28 13:56:39 -0600

    updated home page
    Added image and text

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,13 +3,17 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/footer";
 import Card from "../../components/Card";
 import About from "../about/aboutPage";
-
+//Reference: Chat gpt for css and Tailwind website:https://tailwindcss.com/ for tailwind css
 const Home = () => {
   return (
     <div className="">
       <Navbar />
-      <div className=" ">
-        <img src="/logo.jpg" />
+      <div className="flex justify-between items-center p-4">
+        <h1 className="text-6xl font-bold">
+          Welcome To <br />
+          JK Convenience Store
+        </h1>
+        <img src="/logo.jpg" className="rounded-full w-1/3" />
       </div>
 
       {/* <About /> */}
