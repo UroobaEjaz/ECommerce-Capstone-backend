3b216761fb5ebb22d48a83212537c397cfa4203e
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2024-05-29 11:39:01 -0600

    started chatbot with an example

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -7,3 +7,50 @@ give the information about the help desk */
 
 
 
+
+
+import React from "react";
+import ChatBot from "react-simple-chatbot";
+
+const Contact = () => {
+  const steps = [
+    {
+      id: '1',
+      message: 'Hello! How can I assist you today?',
+      trigger: '2'
+    },
+    {
+      id: '2',
+      message: 'Please select an option:',
+      trigger: 'options'
+    },
+    {
+      id: 'options',
+      options: [
+        { value: 'option1', label: 'Option 1', trigger: '3' },
+        { value: 'option2', label: 'Option 2', trigger: '4' }
+      ]
+    },
+    {
+      id: '3',
+      message: 'You selected Option 1.',
+      end: true
+    },
+    {
+      id: '4',
+      message: 'You selected Option 2.',
+      end: true
+    }
+  ];
+
+  return (
+    <div>
+      <h1>ChatBot Example</h1>
+      <Segment floated= 'right'/>
+      <ChatBot steps={steps} />
+    </div>
+  );
+};
+
+export default Contact;
+
