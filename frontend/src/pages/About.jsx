49093dfa0754df16ef7b1720daa85519164a0739
UroobaEjaz <Urooba.Ejaz@edu.sait.ca>
2024-05-29 10:43:08 -0600

    Corrected the buttons name

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
   const [location, setLocation] = useState(null);
 
   const locations = {
-    SE: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10093.726404129077!2d-114.07675412907968!3d51.002751622735634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170ce0f71aaab%3A0x48a5f8f0f4a4d45a!2s208%20Haddon%20Rd%20SW%2C%20Calgary%2C%20AB%20T2V%202Y6%2C%20Canada!5e0!3m2!1sen!2sus!4v1620306795723!5m2!1sen!2sus",
+    SW: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10093.726404129077!2d-114.07675412907968!3d51.002751622735634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170ce0f71aaab%3A0x48a5f8f0f4a4d45a!2s208%20Haddon%20Rd%20SW%2C%20Calgary%2C%20AB%20T2V%202Y6%2C%20Canada!5e0!3m2!1sen!2sus!4v1620306795723!5m2!1sen!2sus",
     NE: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10642.16057937851!2d-114.0228835969256!3d51.096967660615785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53716fe5c4a91369%3A0x507f6fb42d1b5a0!2s4440%2044%20Ave%20NE%2C%20Calgary%2C%20AB%20T1Y%204K9%2C%20Canada!5e0!3m2!1sen!2sus!4v1620306795723!5m2!1sen!2sus"
   };
 
@@ -27,21 +27,21 @@ export default function About() {
       <div className="flex space-x-4 mb-4">
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded"
-          onClick={() => setLocation('SE')}
+          onClick={() => setLocation('SW')}
         >
-          Show SE Map
+          SW Location
         </button>
         <button
           className="bg-green-500 text-white px-4 py-2 rounded"
           onClick={() => setLocation('NE')}
         >
-          Show NE Map
+          NE location
         </button>
         <button
           className="bg-red-500 text-white px-4 py-2 rounded"
           onClick={() => setLocation(null)}
         >
-          Hide Map
+          Hide map
         </button>
       </div>
       {location && (
