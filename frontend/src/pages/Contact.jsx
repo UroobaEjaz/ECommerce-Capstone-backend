54162acb6f4b763211da085ea158bdacc593dd56
mohjeet3004 <141950821+mohjeet3004@users.noreply.github.com>
2024-06-04 14:20:21 -0600

    tired database
    tired creating local database and displaying items in home screen

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
 import Chat from "./chat.jsx";
-
-
+import Navbar from "../components/Navbar.jsx";
 
 export default function Contact() {
-
   return (
     <div>
-      <Chat/>
+      <Navbar />
+      {/* <Chat /> */}
     </div>
-
   );
-} 
+}
